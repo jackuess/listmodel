660ce384d69d1c75b28c7871dab3f64bf35dec83
Jacques de Laval <chucky@wrutschkow.org>
2013-09-22 17:38:03 +0200

    Implemented RssModel

---
+++ listmodel/models.py
@@ -0,0 +1,32 @@
+from email.utils import mktime_tz, parsedate_tz
+import datetime
+import re
+import time
+from urlparse import urlparse
+
+import listmodel
+
+
+class RssModel(listmodel.XmlListModel):
+    class RssRow(listmodel.Row):
+        link = listmodel.XmlRole('link/text()')
+        text = listmodel.XmlRole('description/text()')
+        title = listmodel.XmlRole('title/text()')
+
+        @listmodel.XmlRole
+        def domain(self, val):
+            domain = urlparse(self.link).netloc
+            domain = re.sub(r'^www\.', '', domain)
+            return domain
+
+        @listmodel.XmlRole('pubDate/text()')
+        def pub_date(self, value):
+            timestamp = mktime_tz(parsedate_tz(value))
+            return datetime.datetime.utcfromtimestamp(timestamp)
+    
+    query = '//item'
+    rowhandler = RssRow
+
+    title = listmodel.XmlRole('//channel/title/text()')
+    fetch_time = listmodel.Role(
+        datetime.datetime.utcfromtimestamp(time.time()))
