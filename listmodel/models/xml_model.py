faa94f6a567b30de03e1ad70d3ae4b02f50d417e
Jacques de Laval <chucky@wrutschkow.org>
2014-04-19 13:45:58 +0200

    Implemented HtmlListModel and HtmlRole

--- listmodel/models/xml_model.py
+++ listmodel/models/xml_model.py
@@ -6,14 +6,29 @@ from .._listmodel import ListModel, Role
 
 
 class XmlListModel(ListModel):
+    __parsercls__ = etree.XMLParser
+    __parseropts__ = {}
+
     def set_context(self, content):
-        self.context = etree.parse(StringIO(content))
+        self.context = etree.parse(StringIO(content),
+                                   self.__parsercls__(**self.__parseropts__))
         self._rows = self.context.xpath(self.__rowquery__)
 
 
 class XmlRole(Role):
     def query(self, obj, xpath):
+        if xpath.startswith('//'):
+            xpath = '.' + xpath
+
         try:
             return obj.context.xpath(xpath)[0]
         except IndexError:
             pass
+
+
+class HtmlListModel(XmlListModel):
+    __parsercls__ = etree.HTMLParser
+
+
+class HtmlRole(XmlRole):
+    pass
